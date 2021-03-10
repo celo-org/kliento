6f129b2525bd9f8ed928556815edbad6b87c8a73
Trevor Porter <trkporter@ucdavis.edu>
2021-03-10 18:53:25 +0000

    nits

--- celotokens/celotokens.go
+++ celotokens/celotokens.go
@@ -10,8 +10,7 @@ import (
     "github.com/celo-org/celo-blockchain/common"
 )
 
-// CeloToken is an int representation of a native token on Celo, like CELO
-// or stable tokens
+// CeloToken is a native token, eg CELO and stable tokens
 type CeloToken string
 
 const (
@@ -23,8 +22,8 @@ const (
 
 type CeloTokenInfo struct {
     contractID registry.ContractID
-    isStableToken bool
     exchangeContractID registry.ContractID
+    isStableToken bool
 }
 
 // CeloTokenInfos contains a CeloTokenInfo entry for each CeloToken key
@@ -35,21 +34,24 @@ var CeloTokenInfos = map[CeloToken]CeloTokenInfo{
     },
     CUSD: CeloTokenInfo{
         contractID: registry.StableTokenContractID,
-        isStableToken: true,
         exchangeContractID: registry.ExchangeContractID,
+        isStableToken: true,
     },
 }
 
+// CeloTokens provides a friendly interface for interacting with Celo tokens
 type CeloTokens struct {
     registry registry.Registry
 }
 
+// New creates a new pointer to a CeloTokens
 func New(reg registry.Registry) (*CeloTokens) {
     return &CeloTokens{
         registry: reg,
     }
 }
 
+// IsStableToken returns whether the provided token is a stable token
 func (ct *CeloTokens) IsStableToken(token CeloToken) bool {
     tokenInfo, ok := CeloTokenInfos[token]
     if !ok {
@@ -58,6 +60,7 @@ func (ct *CeloTokens) IsStableToken(token CeloToken) bool {
     return tokenInfo.isStableToken
 }
 
+// GetExchangeContract gets the exchange contract for a provided stable token
 func (ct *CeloTokens) GetExchangeContract(ctx context.Context, token CeloToken, blockNumber *big.Int) (*contracts.Exchange, error) {
     tokenInfo, ok := CeloTokenInfos[token]
     if !ok {
@@ -73,6 +76,7 @@ func (ct *CeloTokens) GetExchangeContract(ctx context.Context, token CeloToken, 
     return contract.(*contracts.Exchange), nil
 }
 
+// GetStableTokenContract gets the stabletoken contract for a provided stable token
 func (ct *CeloTokens) GetStableTokenContract(ctx context.Context, token CeloToken, blockNumber *big.Int) (*contracts.StableToken, error) {
     tokenInfo, ok := CeloTokenInfos[token]
     if !ok {
@@ -88,6 +92,7 @@ func (ct *CeloTokens) GetStableTokenContract(ctx context.Context, token CeloToke
     return contract.(*contracts.StableToken), nil
 }
 
+// GetContract gets the contract for a provided celo token
 func (ct *CeloTokens) GetContract(ctx context.Context, token CeloToken, blockNumber *big.Int) (contracts.CeloTokenContract, error) {
     tokenInfo, ok := CeloTokenInfos[token]
     if !ok {
@@ -100,6 +105,7 @@ func (ct *CeloTokens) GetContract(ctx context.Context, token CeloToken, blockNum
     return contract.(contracts.CeloTokenContract), nil
 }
 
+// GetExchangeContracts gets the exchange contracts for all stable tokens
 func (ct *CeloTokens) GetExchangeContracts(ctx context.Context, blockNumber *big.Int) (map[CeloToken]*contracts.Exchange, error) {
     exchangeContracts := make(map[CeloToken]*contracts.Exchange)
     for token, tokenInfo := range CeloTokenInfos {
@@ -116,6 +122,8 @@ func (ct *CeloTokens) GetExchangeContracts(ctx context.Context, blockNumber *big
     return exchangeContracts, nil
 }
 
+// GetContracts gets the contracts for all tokens. If onlyStables is true, contracts
+// for only stable tokens are provided.
 func (ct *CeloTokens) GetContracts(ctx context.Context, blockNumber *big.Int, onlyStables bool) (map[CeloToken]contracts.CeloTokenContract, error) {
     contracts := make(map[CeloToken]contracts.CeloTokenContract)
     for token, tokenInfo := range CeloTokenInfos {
@@ -131,6 +139,8 @@ func (ct *CeloTokens) GetContracts(ctx context.Context, blockNumber *big.Int, on
     return contracts, nil
 }
 
+// GetAddresses gets the addresses for all token contracts. If onlyStables is true, addresses
+// for only stable tokens are provided.
 func (ct *CeloTokens) GetAddresses(ctx context.Context, blockNumber *big.Int, onlyStables bool) (map[CeloToken]common.Address, error) {
     addresses := make(map[CeloToken]common.Address)
     for token, tokenInfo := range CeloTokenInfos {
