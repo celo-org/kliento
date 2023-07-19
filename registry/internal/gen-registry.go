f97eaed2c603d4cb32fb9ed716ed77f35481a5a4
Eela Nagaraj <eela@clabs.co>
2023-07-19 11:41:04 +0200

    Update contract generation scripts

--- registry/internal/gen-registry.go
+++ registry/internal/gen-registry.go
@@ -22,6 +22,7 @@ var contractIDToGeneratedContractType = map[string]string{
 	"Exchange":             "Exchange",
 	"ExchangeEUR":          "Exchange",
 	"ExchangeBRL":          "Exchange",
+	"FeeHandler": 			"FeeHandler",
 	"GasPriceMinimum":      "GasPriceMinimum",
 	"GoldToken":            "GoldToken",
 	"Governance":           "Governance",
