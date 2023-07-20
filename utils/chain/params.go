a60e0f3cc1c4f44c635d53e5584e9d5a1a136957
Eela Nagaraj <eela@clabs.co>
2023-07-20 15:02:03 +0200

    Add IsGingerbread to ChainParameters

--- utils/chain/params.go
+++ utils/chain/params.go
@@ -18,11 +18,23 @@ import (
 	"math/big"
 
 	"github.com/celo-org/celo-blockchain/consensus/istanbul"
+	"github.com/celo-org/celo-blockchain/params"
 )
+
+// TODO EN: consider moving this directly to Rosetta
 
 type ChainParameters struct {
 	ChainId   *big.Int
 	EpochSize uint64
+	IsGingerbread func(*big.Int) bool
+}
+
+func NewChainParametersFromConfig(config *params.ChainConfig) *ChainParameters {
+	return &ChainParameters{
+		ChainId: config.ChainID,
+		EpochSize: config.Istanbul.Epoch,
+		IsGingerbread: config.IsGingerbread,
+	}
 }
 
 func (cp *ChainParameters) IsLastBlockOfEpoch(blockNumber uint64) bool {
